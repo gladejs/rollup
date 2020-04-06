28fd11d5e7124311a621d67b9aa263be2dca56d5
Aurélien Stébé <aurelien.stebe@gmail.com>
2020-04-06 15:49:05 -0400

    fix(asset): support Windows paths for image assets
    Use "normalize-path" and strip the drive letter to get an
    import path on Windows (refactored the code a bit too).

--- tags/asset-transformer.js
+++ tags/asset-transformer.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const normalize = require('normalize-path')
 
 module.exports = function transform (el, context) {
   if (el.hasAttribute('src') && el.getAttributeValue('src').type === 'Literal') {
@@ -8,16 +9,18 @@ module.exports = function transform (el, context) {
 
     if (assetPath.startsWith('.')) {
       assetPath = path.resolve(path.dirname(context.filename), assetPath)
+      assetPath = normalize(assetPath).replace(/^([a-zA-Z]+:|\.\/)/, '')
+
       el.setAttributeValue('src', context.builder.literal(assetPath))
 
-      let assetCode = 'import assetPath from \'' + assetPath + '\'; '
-      assetCode += 'assets.push([\'' + assetPath + '\', assetPath]);'
+      let assetCode = "import assetPath from '" + assetPath + "'; "
+      assetCode += "assets.push(['" + assetPath + "', assetPath]);"
 
       context.addDependency({
         type: 'asset',
         code: assetCode,
-        virtualPath: assetPath + '|assets',
-        path: './' + path.basename(context.filename)
+        path: context.filename,
+        virtualPath: assetPath + '|assets'
       })
     }
   }
