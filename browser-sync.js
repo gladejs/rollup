db351510f3aa6b15af6687c3c52ecd4ce05565fd
Aurélien Stébé <aurelien.stebe@gmail.com>
2020-05-07 08:00:23 -0400

    fix(build): put back the "dist" folder properly
    Create JS "link" files for browser-sync & html-minifier so that it
    should work with all versions of NodeJS in commonjs & module mode.

--- browser-sync.js
+++ browser-sync.js
@@ -1,12 +1,3 @@
 'use strict'
 
-module.exports = function (config) {
-  const bs = require('browser-sync').create()
-
-  return {
-    name: 'browser-sync',
-    writeBundle () {
-      if (!bs.active) bs.init(config)
-    }
-  }
-}
+module.exports = require('./dist/browser-sync')
--- html-minifier.js
+++ html-minifier.js
@@ -1,16 +1,3 @@
 'use strict'
 
-module.exports = function (config) {
-  const minify = require('html-minifier').minify
-
-  return {
-    name: 'html-minifier',
-    generateBundle (_, bundle) {
-      Object.values(bundle).filter(entry =>
-        entry.fileName.endsWith('.html')
-      ).forEach(file => {
-        file.source = minify(file.source, config)
-      })
-    }
-  }
-}
+module.exports = require('./dist/html-minifier')
